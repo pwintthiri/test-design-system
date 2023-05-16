ee1d759aea1c983673be4083911612b69349ccb7
Pwint Thiri Ko <pwintthiriko@SIAQ00152MB.local>
2023-05-16 09:08:55 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+export * from './stories/Button';
+export * from './stories/TextInput';
+export * from './stories/Header';
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
